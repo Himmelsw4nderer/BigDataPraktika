1ebd4fb6795bb906cfcb9116f50316275f714780
nbax <nils@nbax.de>
2023-11-02 13:31:57 +0100

    Praktikum 1

---
+++ putinredis.py
@@ -0,0 +1,30 @@
+import json
+import redis
+
+def putBusinesstoRedis(jsonObject: dict):
+    """Puts a business to redis
+
+    Args:
+        jsonObject (dict): The business json object
+    """
+    idKey = f"business:{jsonobject['business_id']}"
+    idPayload = json.dumps(jsonobject)
+    nameKey = f"business:name:{jsonobject['name'].lower()}"
+    namePayload = json.dumps({"id" :jsonobject["business_id"]})
+
+    redisConnection.set(idKey, idPayload)
+    redisConnection.set(nameKey, namePayload)
+
+
+
+redisConnection = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+file = open("businessLarge.json", "r")
+lines = file.readlines()
+for line in lines:
+    jsonobject = json.loads(line)
+    #putBusinesstoRedis(jsonobject)
+    #print(redisConnection.get(nameKey))
+    #print(redisConnection.get(idKey))
+
+
---
+++ searchRedis.py
@@ -0,0 +1,44 @@
+import redis
+import json
+
+
+def search(text: str) -> list:
+    """Search the redis db
+
+    Args:
+        text (str): The search term
+
+    Returns:
+        list: A list of strings containing all possible results
+    """
+    searchTerm = f"business:name:{text.lower()}*"
+    searchResults = redisConnection.keys(searchTerm)
+    searchResults = [searchResult.split(":")[2] for searchResult in searchResults]
+    return searchResults
+
+
+def getByCompanieName(companyName: str) -> str:
+    """Looks in the redis db for the company and returns the corallation value
+
+    Args:
+        key (str): The name of the company
+
+    Returns:
+        str: The value
+    """
+    key = f"business:name:{companyName}"
+    key2 = json.loads(redisConnection.get(key))["id"]
+    return redisConnection.get(f"business:{key2}")
+
+redisConnection = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+text = "null"
+while(text != "quit"):
+    text = input("Geben sie ein Suchbegriff ein: ")
+    results = search(text)
+    print("Press the corralating button to get more infos: ")
+    for i, result in enumerate(results):
+        print(f"\t {i+1} -> {result}")
+    result = int(input("Input the corralating button to get more infos: "))
+    company = getByCompanieName(results[result-1])
+    print(f"Address: {company['address']} {company['postal_code']} {company['city']} {company['state']} \nEvaluation: {company['stars']}")
